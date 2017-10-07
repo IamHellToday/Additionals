ab1e01215c960ca7de8ee596c531fa2524db257f
Krystian Jabłoński <Krystian Jaboski>
2017-10-07 02:43:08 +0200

    fixes

--- scripts.js
+++ scripts.js
@@ -1,13 +1,12 @@
-var button = document.querrySelector('button');
-    button.addEventListener('click', function() {
-        if(button.textContent == 'ON') {
-            button.textContent = 'OFF';
-        }
-        else{
-            button.textContegint = 'ON'};
-        }
-        var letters = document.querySelectorAll('li');
-            for(var i = 0; letters.length > i; i++){
-                letters[i].classList.toggle('off');
-            }
-    });
+var button = document.querySelector("button");
+    button.addEventListener("click", function() {
+  if (this.textContent == "ON") {
+    this.textContent = "OFF";
+  } else {
+    this.textContent = "ON";
+  }
+  const letters = document.querySelectorAll("li");
+    for (var i = 0; letters.length > i; i++) {
+        letters[i].classList.toggle("off");
+  }
+})
