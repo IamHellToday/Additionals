ed99ceb0d8205b2e15136d6a179ee3d29555ffd3
Krystian Jabłoński <Krystian Jaboski>
2017-10-17 14:41:15 +0200

    Initial

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
-var button = document.querySelector("button");
-    button.addEventListener("click", function() {
-  if (this.textContent == "ON") {
-    this.textContent = "OFF";
-  } else {
-    this.textContent = "ON";
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; //Off-switch for the function if there  is no audio.
+    audio.currentTime = 0; //Auto-rewind
+    audio.play();
+    key.classList.add('playing');
   }
-  const letters = document.querySelectorAll("li");
-    for (var i = 0; letters.length > i; i++) {
-        letters[i].classList.toggle("off");
+  function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
   }
-})
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
