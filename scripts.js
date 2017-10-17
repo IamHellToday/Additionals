45ca7e71e994babacbf24971292fbfb826a6c8b9
Krystian Jabłoński <Krystian Jaboski>
2017-10-17 14:46:55 +0200

    fixes

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
   function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return; //Off-switch for the function if there  is no audio.
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return; //Off-switch for the function if there  is no audio.
+
     audio.currentTime = 0; //Auto-rewind
-    audio.play();
     key.classList.add('playing');
+    audio.play();
   }
   function removeTransition(e) {
-    if(e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
   }
-const keys = document.querySelectorAll('.key');
+const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
 
